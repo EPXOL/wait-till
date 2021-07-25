7b059e3517bc906f7077d1f6d57c4abedeec9676
EPXOL <61593333+EPXOL@users.noreply.github.com>
2021-07-25 15:29:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-//Export function
 module.exports = (Variable, Function, Callback) => {
-  //If callback is present make looping checks
   if(Callback) {
     var interval = setInterval(() => {
-      //Match variable in function
       if(Function(Variable)) {
-        //Run callback
         Callback();
       }
     })
@@ -14,15 +10,10 @@ module.exports = (Variable, Function, Callback) => {
         clearInterval(interval);
       }
     }
-  } //when Callback is not presend create one time match check that returns true
-  else {
-    //Create Promise
+  } else {
     return new Promise((resolve, reject) => {
-      //Create Interval to match the variable
       var interval = setInterval(() => {
-        //Match variable in function
         if(Function(Variable)) {
-          //Clear Interval so it will stop checking and resolve "true"
           clearInterval(interval);
           resolve(true);
         }
